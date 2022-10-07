d30c47f923e1b12e98f66050e075a22bd0ad6a15
David Sena <sena.ufc@gmail.com>
2022-10-06 21:31:17 -0300

    up

--- base/009/.cache/draft.java
+++ base/009/.cache/draft.java
@@ -36,7 +36,7 @@ class Trampoline{
     public Trampoline() { //todo
     }
 
-    private Kid remove_kid(String name, LinkedList<Kid> list) { //todo
+    private Kid removeFromList(String name, LinkedList<Kid> list) { //todo
     }
 
     public void arrive(Kid kid) { //todo
@@ -48,7 +48,7 @@ class Trampoline{
     public void leave() { //todo
     }
 
-    public Kid remove(String name) { //todo
+    public Kid remoteKid(String name) { //todo
     }
     public String toString() {
         return   "[" + waiting.stream().map(Kid::toString).collect(Collectors.joining(", ")) + "]" + " => "
@@ -73,8 +73,8 @@ class Solver {
                 tramp.enter();
             } else if(ui[0].equals("leave")) {
                 tramp.leave();
-            } else if(ui[0].equals("remove")) {//name
-                tramp.remove(ui[1]);
+            } else if(ui[0].equals("remoteKid")) {//name
+                tramp.remoteKid(ui[1]);
             } else if(ui[0].equals("show")) {
                 System.out.println(tramp);
             } else {
